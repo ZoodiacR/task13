40a55da1f78fc1ef69af111cd5814da37285bbaf
ZoodiacR <andyleon000820@gmail.com>
2024-07-03 13:06:09 -0400

    Update urls.py

--- Task/urls.py
+++ Task/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from tareas.views import TaskViewSet
 from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet)
@@ -29,9 +29,7 @@ urlpatterns = [
     path('', include(router.urls)),
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+urlpatterns += staticfiles_urlpatterns()
 
 
 
